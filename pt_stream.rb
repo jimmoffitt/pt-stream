81e1dbcc0e47069437e0171c66f2af9a003dc858
Jim Moffitt <jmoffitt@gnipcentral.com>
2014-02-13 13:24:22 -0700

    Added logging and output to stdout

--- pt_stream.rb
+++ pt_stream.rb
@@ -11,15 +11,13 @@ local database.  The Rails application will ride on top of this database.
 =end
 
 require_relative "./http_stream"  #based on gnip-stream project at (https://github.com/rweald/gnip-stream).
+require_relative "./pt_activity"
 require 'base64'
-require 'active_record'
 require 'mysql2'
 require 'time'
 require 'optparse'
+require 'logger'
 
-class Activity < ActiveRecord::Base
-    validates :native_id, uniqueness: {scope: :publisher}
-end
 
 class PtStream
 
@@ -27,7 +25,10 @@ class PtStream
                   :publisher, :stream_type, :stream_label,
                   :url,
                   :database_adapter, :db_host, :db_schema, :db_user_name, :db_password,
-                  :activities
+                  :activities,
+                  :output,
+                  :log, :log_level, :log_file
+
 
     def initialize(config)
 
@@ -36,6 +37,22 @@ class PtStream
         if not config.nil? then
             getConfig(config)
         end
+
+        #Set up logger.
+        @log = Logger.new(@log_file,10,1024000) #Roll log file at 1 MB, save ten.
+        if @log_level == 'debug' then
+            Logger::DEBUG
+        elsif @log_level == 'info'
+            Logger::INFO
+        elsif @log_level == 'warn'
+            Logger::WARN
+        elsif @log_level == 'error'
+            Logger::ERROR
+        elsif @log_level == 'fatal'
+            Logger::FATAL
+        end
+
+        @log.info { "Creating Streaming Client object with config file: " + config }
     end
 
     def getPassword
@@ -57,8 +74,13 @@ class PtStream
         @publisher = config['stream']['publisher']
         @stream_type = config['stream']['type']
         @stream_label = config['stream']['label']
+        @output = config['stream']['output']
 
         @url = setURL
+
+        #Set logging details.
+        @log_level = config['logging']['log_level']
+        @log_file = config['logging']['log_file']
 
         #Set database details.
         @database_adapter = config['database']['adapter']
@@ -93,6 +115,7 @@ class PtStream
 
         if @publisher == "tumblr" then
             native_id = id.split("/")[-2]
+            p native_id
         end
 
         #These comment/like/vote streams have a blog ID, post ID and a comment ID, so I decided to capture all three.
@@ -127,7 +150,7 @@ class PtStream
         end
 
         if @publisher == "estimize" then
-            native_id = id.split("/")[-1]
+            native_id = id.split(":")[-1]
         end
 
 
@@ -222,12 +245,27 @@ class PtStream
         if @publisher == "newsgator" or (@publisher.include?("wordpress") and @stream_type == "post") then
             body = data["object"]["content"]
         elsif @publisher == "getglue" then
-             verb = data["verb"]
+             verb = data["verb"]  #GetGlue offers a field that sums up the activity based on the verb
+
+             if verb == "user_protect" then
+                 p "ignore"
+             end
+
+
              if verb == "share" then
                  body = data["body"]
              else #verb == "vote" or verb == "post" or verb == "like" or verb == "dislike" or verb == "follow" or verb == "add" or verb == "receive" or verb == "checkin" or verb == "reject" or "user_protect" then
                  body = data["displayName"]
              end
+        elsif @publisher == "estimize" then
+            body = data["target"] #TODO: this needs to be flushed out!  Need to construct the "body" from many parts...
+        elsif @publisher == "tumblr" then
+
+            if not data["object"]["summary"].nil? then
+                body = data["object"]["summary"]
+            else
+                body = data["object"]["content"]
+            end
         else
             body = data["body"]
         end
@@ -236,25 +274,36 @@ class PtStream
     end
 
     '''
-    Parses normalized Activity Stream JSON.
+    Parses normalized PtActivity Stream JSON.
     Parsing details here are driven by the current database schema used to store activities.
     If writing files, then just write out the entire activity payload to the file.
     '''
     def processResponseJSON(activity)
 
-        p activity
+        log.debug 'Entering processResponseJSON'
 
-        if activity.include?("delete") then   #TODO: Need to test on VERB, not content.
+        if @output == 'stdout' then
+            puts activity
+            return #all done here, not writing to db.
+        end
+
+        if @publisher =="tumblr" and activity.include?("delete") then   #TODO: Need to test on VERB, not content.
             activity.gsub!('\"','"')   #Testing and tripping over Tumblr delete activities prompted the special
             #handling of the activity JSON format.
         end
 
         begin
             data = JSON.parse(activity)
+            #p "Activity parsed: #{activity}"
         rescue => e
-            p e.message
-            p e.backtrace
-            return #Could not parse the activity, move on to next... Would want to log the offending activity...
+            @log.error { "Activity NOT parsed: #{activity}"}
+            @log.error { "Error msg: #{e.message} || Backtrace: #{e.backtrace}"}
+
+            if @log_level == 'debug' then
+                p "Activity NOT parsed: #{activity}"
+                p "See log file at #{@log_file} for backtrace"
+            end
+            return #Could not parse the activity, move on to next...
         end
 
         #It is wise to store the entire activity payload, in case you need to parse it later...
@@ -287,7 +336,7 @@ class PtStream
 
         begin
 
-            p native_id + " --> " + body
+            @log.debug { native_id + " --> " + body }
 
             Activity.create(:native_id => native_id,
                             :publisher => @publisher,
@@ -303,26 +352,33 @@ class PtStream
                             :stream_id => stream_id
             )
         rescue => e
-            p e.message
-            p e.backtrace
+            @log.error { "Failed to write to database.  Error: #{e.message} | Backtrace: #{e.backtrace}" }
+            if @log_level == 'debug' then
+                p "Failed to write to database.  Error: #{e.message} | Backtrace: #{e.backtrace}"
+            end
         end
-
-        p "Activity added to database..."
-
     end
 
     #There is one thread for streaming/consuming data, and it calls this.
     def consumeStream(stream)
         begin
             stream.consume do |message|
+
+                #message.force_encoding("UTF-8")
+
                 @activities << message #Add to end of array.
-                if @activities.length > 1000 then
-                    "Queueing #{@activities.length} activities..."
+
+                if message.scan(/"gnip":/).count > 1 then
+                    @log.warn { "Received corrupt JSON? --> #{message}" }
                 end
-                #puts "#{message}"
+
+                if @activities.length > 1000 then
+                    @log.debug "Queueing #{@activities.length} activities..."
+                end
+
             end
         rescue => e
-            p "Error occurred: #{e.message}"
+            @log.error { "Error occurred in consumeStream: #{e.message}" }
             consumeStream(@stream)
         end
     end
@@ -332,9 +388,14 @@ class PtStream
         while true
             while @activities.length > 0
                 activity = @activities.shift  #FIFO, popping from start of array.
+
+                if activity.scan(/"gnip":/).count > 1 then
+                    @log.warn { "Received corrupt JSON? --> #{activity}" }
+                end
+
                 processResponseJSON(activity)
             end
-            sleep (1)
+            sleep (0.5)
         end
     end
 
@@ -346,14 +407,15 @@ class PtStream
 
         @stream = PowertrackStream.new(@url,@user_name, @password)
 
-        t = Thread.new {Thread.pass; consumeStream(stream)}
+        #t = Thread.new {Thread.pass; consumeStream(stream)}
+        t = Thread.new {consumeStream(stream)}
 
         begin
             t.run
         rescue ThreadError => e
-            p "Threading error: #{e.message}"
+            @log.error { "Error starting consumer thread: #{e.message}" }
         rescue => e
-            p "Error: #{e.message}"
+            @log.error { "Error starting consumer thread: #{e.message}" }
         end
 
         threads << t  #Add it to our pool (array) of threads.
@@ -365,18 +427,19 @@ class PtStream
         begin
             t.run
         rescue ThreadError => e
-            p e.message
+            @log.error { "Error starting storeActivity thread: #{e.message}" }
         end
 
         threads << t #Add it to our pool (array) of threads.
 
         threads.each do |t|
             begin
+                @log.debug('here')
                 t.join
             rescue ThreadError => e
-                p "Threading error: #{e.message}"
+                @log.error { "Error with thread join: #{e.message}" }
             rescue => e
-                p "Error: #{e.message}"
+                @log.error { "Error with thread join: #{e.message}" }
             end
         end
 
@@ -396,8 +459,6 @@ if __FILE__ == $0  #This script code is executed when running this file.
     if $config.nil? then
         $config = "./config_private.yaml"  #Default
     end
-
-    p "Creating Streaming Client object with config file: " + $config
 
     pt = PtStream.new($config)
     pt.streamData
